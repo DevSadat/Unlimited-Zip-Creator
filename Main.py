9430fc0447689d60a7044abc4e0ac726d12125df
DevSadat <99319970+DevSadat@users.noreply.github.com>
2022-02-10 10:42:43 +0600

    Create Main.py
    Run this

---
+++ Main.py
@@ -0,0 +1,45 @@
+import zipfile
+import random
+my_zip = zipfile.ZipFile('files.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files2.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files3.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files4.zip', 'w')
+
+my_zip.close()
+my_zip = zipfile.ZipFile('files5.zip', 'w')
+
+my_zip.close()
+my_zip = zipfile.ZipFile('files6.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files7.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files8.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files9.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile('files10.zip', 'w')
+
+my_zip.close()
+
+my_zip = zipfile.ZipFile(' ', 'w')
+
+my_zip.close()
+
+print("Done. https://Alphabrawl.cf/discord")
